af09db47d7cfccde0e2d0f40d848fcf244285346
Marcus Dammann <m.dammann@ams-erp.com>
2021-01-27 17:22:15 +0100

    Starting with networking

--- src/WebSite/Controllers/TutorialController.cs
+++ src/WebSite/Controllers/TutorialController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebSite.Controllers
 {
@@ -14,6 +10,21 @@ namespace WebSite.Controllers
         }
 
         public IActionResult Persistence()
+        {
+            return View();
+        }
+
+        public IActionResult Persistence1()
+        {
+            return View();
+        }
+        
+        public IActionResult Persistence2()
+        {
+            return View();
+        }
+
+        public IActionResult MultiContainer()
         {
             return View();
         }
