55561ed7cd6a1f55ade0ed371bcac6addf48c95a
Marcus Dammann <m.dammann@ams-erp.com>
2021-01-28 12:08:29 +0100

    Implementing resilient migration

--- src/WebSite/Program.cs
+++ src/WebSite/Program.cs
@@ -1,7 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Polly;
 using WebSite.Data;
 
 namespace WebSite
@@ -12,14 +15,28 @@ namespace WebSite
         {
             var host = CreateHostBuilder(args).Build();
 
+            var retry = Policy.Handle<SqlException>()
+                .WaitAndRetry(new TimeSpan[]
+                {
+                    TimeSpan.FromSeconds(3),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(8),
+                    TimeSpan.FromSeconds(20),
+                    TimeSpan.FromSeconds(30),
+                });
+            retry.Execute(() => InvokeMigration(host));
+
+            host.Run();
+        }
+
+        private static void InvokeMigration(IHost host)
+        {
             using (var scope = host.Services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetService<ToDoListDbContext>();
-        
+
                 db.Database.Migrate();
             }
-
-            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
